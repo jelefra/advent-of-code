59f894c05994b34a2a0a80f32f15a5cab5377a49
Jeremy Le François <jelefra@users.noreply.github.com>
2021-12-02 19:04:46 +0000

    Complete day 2

---
+++ src/2021/02.js
@@ -0,0 +1,61 @@
+// eslint-disable-next-line no-unused-vars
+const { getInput, time } = require('../helpers.js');
+
+const input = getInput('./src/2021/02-input.txt');
+
+const part1 = () => {
+  const instructions = input.reduce(
+    (instructions, instruction) => {
+      const [direction, valueAsString] = instruction.split(' ');
+      const value = Number(valueAsString);
+
+      switch (direction) {
+        case 'forward':
+          instructions.horizontal += value;
+          break;
+        case 'up':
+          instructions.depth -= value;
+          break;
+        case 'down':
+          instructions.depth += value;
+          break;
+      }
+      return instructions;
+    },
+    { horizontal: 0, depth: 0 }
+  );
+
+  return instructions.horizontal * instructions.depth;
+};
+// console.log('Part 1: ', time(part1));
+// 0.48 ms
+
+const part2 = () => {
+  const instructions = input.reduce(
+    (instructions, instruction) => {
+      const [direction, valueAsString] = instruction.split(' ');
+      const value = Number(valueAsString);
+
+      switch (direction) {
+        case 'forward':
+          instructions.horizontal += value;
+          instructions.depth += instructions.aim * value;
+          break;
+        case 'up':
+          instructions.aim -= value;
+          break;
+        case 'down':
+          instructions.aim += value;
+          break;
+      }
+      return instructions;
+    },
+    { horizontal: 0, depth: 0, aim: 0 }
+  );
+
+  return instructions.horizontal * instructions.depth;
+};
+// console.log('Part 2: ', time(part2));
+// 0.48 ms
+
+module.exports = { part1, part2 };
--- src/2021/test.js
+++ src/2021/test.js
@@ -1,4 +1,5 @@
 const day1 = require('./01.js');
+const day2 = require('./02.js');
 
 describe('Advent of Code 2021', () => {
   describe('day 1', () => {
@@ -10,6 +11,18 @@ describe('Advent of Code 2021', () => {
     it('passes part 2', () => {
       const answer = day1.part2();
       expect(answer).toEqual(1706);
+    });
+  });
+
+  describe('day 2', () => {
+    it('passes part 1', () => {
+      const answer = day2.part1();
+      expect(answer).toEqual(1728414);
+    });
+
+    it('passes part 2', () => {
+      const answer = day2.part2();
+      expect(answer).toEqual(1765720035);
     });
   });
 });
