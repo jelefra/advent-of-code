05b5eaa317ee83f144ab98c481c7dfc9360efb12
Jeremy Le François <jelefra@users.noreply.github.com>
2021-12-03 19:44:02 +0000

    Complete day 3

---
+++ src/2021/03.js
@@ -0,0 +1,76 @@
+// eslint-disable-next-line no-unused-vars
+const { getInput, time, convertBinaryToDecimal } = require('../helpers.js');
+
+const input = getInput('./src/2021/03-input.txt');
+
+const part1 = () => {
+  const rates = input
+    .reduce((bitsByIndex, binary) => {
+      binary.split('').forEach((bit, index) => {
+        bitsByIndex[index] = bitsByIndex[index] || { 0: 0, 1: 0 };
+        bitsByIndex[index][bit]++;
+      });
+      return bitsByIndex;
+    }, [])
+    .reduce(
+      (rates, bits) => {
+        // Unsure what to do in case of equality
+        // Equality would be possible considering even count of input numbers
+        // Does not affect the solution given supplied input
+        if (bits[0] > bits[1]) {
+          rates.gamma += '0';
+          rates.epsilon += '1';
+        } else {
+          rates.gamma += '1';
+          rates.epsilon += '0';
+        }
+        return rates;
+      },
+      { gamma: '', epsilon: '' }
+    );
+
+  return (
+    convertBinaryToDecimal(rates.gamma) * convertBinaryToDecimal(rates.epsilon)
+  );
+};
+// console.log('Part 1: ', time(part1));
+// 0.32 ms
+
+const findMostCommonBit = (numbers, index) => {
+  const bits = numbers.reduce(
+    (acc, cv) => {
+      cv[index] === '0' ? acc[0]++ : acc[1]++;
+      return acc;
+    },
+    { 0: 0, 1: 0 }
+  );
+  return bits[1] >= bits[0] ? 1 : 0;
+};
+
+const findLeastCommonBit = (numbers, index) =>
+  findMostCommonBit(numbers, index) === 1 ? 0 : 1;
+
+const filterNumbers = (numbers, criteria, position = 0) => {
+  const filteringValue = criteria(numbers, position);
+  const filteredNumbers = numbers.filter(
+    (number) => Number(number[position]) === filteringValue
+  );
+
+  return filteredNumbers.length > 1
+    ? filterNumbers(filteredNumbers, criteria, position + 1)
+    : filteredNumbers[0];
+};
+
+const part2 = () => {
+  const oxygenGeneratorRating = filterNumbers(input, findMostCommonBit);
+  const CO2ScrubberRating = filterNumbers(input, findLeastCommonBit);
+
+  return (
+    convertBinaryToDecimal(oxygenGeneratorRating) *
+    convertBinaryToDecimal(CO2ScrubberRating)
+  );
+};
+// console.log('Part 2: ', time(part2));
+// 0.76 ms
+
+module.exports = { part1, part2 };
--- src/2021/test.js
+++ src/2021/test.js
@@ -1,5 +1,6 @@
 const day1 = require('./01.js');
 const day2 = require('./02.js');
+const day3 = require('./03.js');
 
 describe('Advent of Code 2021', () => {
   describe('day 1', () => {
@@ -23,6 +24,18 @@ describe('Advent of Code 2021', () => {
     it('passes part 2', () => {
       const answer = day2.part2();
       expect(answer).toEqual(1765720035);
+    });
+  });
+
+  describe('day 3', () => {
+    it('passes part 1', () => {
+      const answer = day3.part1();
+      expect(answer).toEqual(3277364);
+    });
+
+    it('passes part 2', () => {
+      const answer = day3.part2();
+      expect(answer).toEqual(5736383);
     });
   });
 });
