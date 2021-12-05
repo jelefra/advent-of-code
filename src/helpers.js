01674d9ca2025f475458358e6508a8dda59cf8e7
Jeremy Le François <jelefra@users.noreply.github.com>
2021-12-05 23:08:49 +0000

    Allow to set the number of repetitions

--- src/helpers.js
+++ src/helpers.js
@@ -4,24 +4,25 @@ const REPETITIONS = 1000;
 const getInput = (path, split = '\n') =>
   require('fs').readFileSync(path, 'utf8').split(split).filter(Boolean);
 
-const runRepeatedly = (func) => {
-  for (let i = 0; i < REPETITIONS; i++) {
+const runRepeatedly = (func, repetitions) => {
+  for (let i = 0; i < repetitions; i++) {
     func();
   }
 };
 
 const getCurrentTime = () => new Date().getTime();
 
-const measureExecutionTime = (func) => {
+const measureExecutionTime = (func, repetitions) => {
   const startTime = getCurrentTime();
-  runRepeatedly(func);
+  runRepeatedly(func, repetitions);
   const endTime = getCurrentTime();
-  return (endTime - startTime) / REPETITIONS;
+  return (endTime - startTime) / repetitions;
 };
 
 const formatDuration = (ms) => `${ms.toFixed(2)} ms`;
 
-const time = (fn) => formatDuration(measureExecutionTime(fn));
+const time = (fn, repetitions = REPETITIONS) =>
+  formatDuration(measureExecutionTime(fn, repetitions));
 
 const convertBinaryToDecimal = (binary) => parseInt(binary, 2);
 
