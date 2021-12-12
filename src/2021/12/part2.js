e458e32d16e0c54586aaf7ab366703a5b2b9fcf8
Jeremy Le François <jelefra@users.noreply.github.com>
2021-12-12 12:15:39 +0000

    Complete day 12

---
+++ src/2021/12/part2.js
@@ -0,0 +1,71 @@
+// eslint-disable-next-line no-unused-vars
+const { getInput, time } = require('../../helpers.js');
+
+const input = getInput('./src/2021/12/input.txt');
+
+const optionIsSmallCaveThatHasBeenVisited = (path, option) =>
+  path.includes(option) && option.toLowerCase() === option;
+
+const hasVisitedASmallCaveTwice = (path, smallCaves) =>
+  smallCaves.filter(
+    (smallCave) => path.filter((cave) => cave === smallCave).length > 1
+  ).length > 0;
+
+const isDeadEnd = (path, option, smallCaves) =>
+  hasVisitedASmallCaveTwice(path, smallCaves) &&
+  optionIsSmallCaveThatHasBeenVisited(path, option, smallCaves);
+
+const exploreNextPath = (path, connections, smallCaves) => {
+  const options = connections[path.slice(-1)[0]];
+  const nextPaths = [];
+  let counter = 0;
+  for (let option of options) {
+    if (option === 'end') {
+      counter++;
+    } else if (!isDeadEnd(path, option, smallCaves)) {
+      const continuedPath = [...path];
+      continuedPath.push(option);
+      nextPaths.push(continuedPath);
+    }
+  }
+
+  return { nextPaths, counter };
+};
+
+const determineLinks = (connections) =>
+  connections.reduce((links, segment) => {
+    const [from, to] = segment.split('-');
+
+    links[from] = links[from] || [];
+    !links[from].includes(to) && to !== 'start' && links[from].push(to);
+
+    links[to] = links[to] || [];
+    !links[to].includes(from) && from !== 'start' && links[to].push(from);
+
+    return links;
+  }, {});
+
+const countPaths = (connections) => {
+  const links = determineLinks(connections);
+  const incompletePaths = [['start']];
+  let countOfCompletePaths = 0;
+
+  const smallCaves = Object.keys(links).filter(
+    (cave) => cave !== 'start' && cave !== 'end' && cave.toLowerCase() === cave
+  );
+
+  while (incompletePaths.length) {
+    const nextPath = incompletePaths.shift();
+    const { nextPaths, counter } = exploreNextPath(nextPath, links, smallCaves);
+    incompletePaths.push(...nextPaths);
+    countOfCompletePaths += counter;
+  }
+
+  return countOfCompletePaths;
+};
+
+const part2 = (connections = input) => countPaths(connections);
+// console.log('Part 2: ', time(part2, 3));
+// 21.47s
+
+module.exports = { part2 };
